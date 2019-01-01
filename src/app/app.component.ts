9b3a4962e991aa0c29f5ea6b4da725f4cb50128c
VinayakDimble27 <vinayakdimble96@gmail.com>
2019-01-01 17:03:54 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`.appclr{color:blue!important;}`]
+})
+export class AppComponent {
+  title = 'Marvellous Infosystems';
+}
